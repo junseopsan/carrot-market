3033ef63fefed915b5d0126ad4076a1c761def99
junseopsan <junseopsan@gmail.com>
2022-09-09 21:37:42 +0900

    #4.2 Test Drive part Two

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,10 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div className="grid gap-10 bg-slate-400 py-20 px-10">
-      <div className="rounded-3xl bg-white p-6 shadow-xl">
+    <div className="grid gap-10 px-10 py-20 bg-slate-400">
+      <div className="p-6 bg-white shadow-xl rounded-3xl">
         <span className="text-3xl font-semibold">Select Item</span>
-        <div className="my-2 flex justify-between">
+        <div className="flex justify-between my-2">
           <span className="text-gray-500">Grey Chair</span>
           <span className="font-semibold">$19</span>
         </div>
@@ -13,21 +13,40 @@ const Home: NextPage = () => {
           <span className="text-gray-500">Grey Chair</span>
           <span className="font-semibold">$19</span>
         </div>
-        <div className="mt-2 flex justify-between border-t-2 border-dashed pt-2">
+        <div className="flex justify-between pt-2 mt-2 border-t-2 border-dashed">
           <span>Total</span>
           <span className="font-semibold">$10</span>
         </div>
         <div
-          className="mx-auto mt-5 w-2/4 rounded-xl
-          bg-blue-500 p-3 text-center text-white
-         "
+          className="w-2/4 p-3 mx-auto mt-5 text-center text-white bg-blue-500 rounded-xl "
         >
           Checkout
         </div>
       </div>
-      <div className="rounded-2xl bg-white p-10 shadow-xl"></div>
-      <div className="rounded-2xl bg-white p-10 shadow-xl"></div>
-      <div className="rounded-2xl bg-white p-10 shadow-xl"></div>
+      <div className="overflow-hidden bg-white shadow-xl rounded-3xl">
+        <div className="p-6 bg-blue-500 pb-14">
+          <span className="text-2xl text-white">Profile</span>
+        </div>
+        <div className="relative p-6 bg-white rounded-3xl -top-5">
+          <div className="relative flex items-end justify-between -top-16">
+            <div className="flex flex-col items-center">
+              <span className="text-sm text-gray-500">Orders</span>
+              <span className="font-medium">340</span>
+            </div>
+            <div className="w-24 h-24 bg-red-400 rounded-full" />
+            <div className="flex flex-col items-center">
+              <span className="text-sm text-gray-500">Spent</span>
+              <span className="font-medium">$340</span>
+            </div>
+          </div>
+          <div className="relative flex flex-col items-center -mt-10 -mb-5">
+            <span className="text-lg font-medium">Tony Molloy</span>
+            <span className="text-sm text-gray-500">미국</span>
+          </div>
+        </div>
+      </div>
+      <div className="p-6 bg-white shadow-xl rounded-3xl"></div>
+      <div className="p-6 bg-white shadow-xl rounded-3xl"></div>
     </div>
   );
 };
